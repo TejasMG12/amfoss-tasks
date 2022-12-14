2dd5e267bea01a7964c4c94aff42f975eff9be75
Tejas Gampawar <tejasgampawar12@gmail.com>
2022-12-14 18:49:23 +0530

    ..

---
+++ task-09/README.md
@@ -0,0 +1,2 @@
+I did attempt it.
+Read about rust and started doing it but didn't got much time to complete it.
--- task-15/task-15-2.py
+++ task-15/task-15-2.py
@@ -1,14 +1,19 @@
 t=int(input())
 for i in range (0,t):
     n=int(input())
-    list=[1,2]
-    y=0
-    while list[len(list)-1]<n:
-        a=list[len(list)-1]
-        b=list[len(list)-2]
-        if (a+b)<n:
-            list.append(a+b)
-            if (a+b)%2==0:
-                y=y+a+b
-    print(y)
+    n1,n2=1,2
+    y=2
+    while n1+n2<n:
+        
+        nth= n1 + n2
+        n1=n2
+        n2=nth
+        if nth%2==0:
+            y=y+nth
+        
+    if n>1:       
+        print(y)
+    else:
+        print(0)
+
 
